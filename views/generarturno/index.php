3678165928759df60807f21b49590875e0a4cea3
Moises-Pena <moises9798@gmail.com>
2024-12-11 19:23:33 -0400

    main

---
+++ views/generarturno/index.php
@@ -0,0 +1,58 @@
+<?php
+require "../template/header.php";
+?>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+  <div class="card mb-4">
+    <h5 class="card-header">Generar Turno</h5>
+    <div class="card-body">
+
+      <div class="row mb-4">
+      <div class="col-4">
+          <select id="tipodocumento" name="tipodocumento" class="form-select">
+            <option value="" readonly>Seleccionar Tipo Documento</option>
+            <option value="CC">Código de empleado</option>
+          </select>
+        </div>
+        <div class="col-4">
+          <input type="text" id="numerodocumento" name="numerodocumento" class="form-control" placeholder="Código de empleado" />
+        </div>
+        <div class="col-4">
+          <button type="button" Onclick="BuscarCliente()" class="btn btn-primary float-right">Buscar</button>
+        </div>
+      </div>
+      <div id="verdatoscliente">
+      <div class="row">
+        <div class="col-6 mb-2">
+          <label class="form-label">Documento</label>
+          <select id="documento" name="documento" class="form-select">
+            <option value="" disabled>Seleccionar</option>
+            <option value="CC">Código de empleado</option>            
+          </select>
+        </div>
+        <div class="col-6 mb-2">
+          <label class="form-label">Número</label>
+          <input type="text" id="numero" name="numero" class="form-control" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 mb-2">
+          <label class="form-label">Nombres</label>
+          <input type="text" id="pnombre" name="pnombre" class="form-control" />
+        </div>
+        <div class="col-3 mb-2">
+          <label class="form-label">Apellidos</label>
+          <input type="text" id="papellido" name="papellido" class="form-control" />
+        </div>
+      </div>
+        <div id="contedinoservicios" class="row mt-4">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+require "../template/footer.php";
+?>
+<script src="../../controllers/generarturnoController.js"></script>
